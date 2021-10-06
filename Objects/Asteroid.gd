d825329ff76d0ce3692f9e7eaf27a1f704f0ed8c
lohare21 <lohare@iu.edu>
2021-10-06 18:05:03 -0400

    Game

--- Objects/Asteroid.gd
+++ Objects/Asteroid.gd
@@ -5,10 +5,12 @@ var is_exploded = false
 func explode(): 
 	if is_exploded:
 		return
-		
 	is_exploded = true
 	
 	get_parent().remove_child(self)
 	queue_free()
+
+signal score_changed
+var score_value = 100
 
 
